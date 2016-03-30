9a3768c7a719d1438ec25984ae49eb87da286b60
Rails Udemy Course <railsudemycourse@gmail.com>
2016-03-30 18:30:48 +0000

    Add user meta details to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,6 +20,7 @@ class ArticlesController < ApplicationController
   end
   
   def update
+    @article.user = User.first
     if @article.update(article_params)
       flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
